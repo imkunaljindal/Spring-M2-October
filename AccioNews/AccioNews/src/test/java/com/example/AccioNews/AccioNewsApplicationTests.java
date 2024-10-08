49e9c90d5c67015ee6df0a76262ed80752cf52d9
Kunal Jindal <kunaljindal@microsoft.com>
2024-10-08 22:02:28 +0530

    Add News Service API

---
+++ AccioNews/AccioNews/src/test/java/com/example/AccioNews/AccioNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccioNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccioNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
